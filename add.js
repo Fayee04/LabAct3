3f5dcb07605bcdf65421ab53d02762937ea55ef4
Fayee04 <KristineTuazon16@gmail.com>
2024-07-22 14:31:24 +0800

    Create add.js

---
+++ add.js
@@ -0,0 +1,8 @@
+var add = require('./app.js');
+
+const num1 = 11;
+const num2 = 10;
+
+const result = add(num1, num2);
+
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + result + ' ');
